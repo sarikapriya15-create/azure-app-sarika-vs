a46e1f00f4a69e30801913a2622336bfedbe2848
Amit Ranjan Das <amitranjan.das21@outlook.com>
2026-04-19 15:19:34 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using azure_app_sarika_vs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace azure_app_sarika_vs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azure_app_sarika_vs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
